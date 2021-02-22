ba12f1882b4849e33ca7f51c7643de7854151f53
Fabien AGNUS <fabien.agnus@sncf.fr>
2021-02-22 15:40:45 +0100

    IMPL: first version 01 Spurs article

---
+++ js/articles/01_san_antonio_spurs/main.js
@@ -0,0 +1,4 @@
+function main() {
+    console.log("hello")
+    $("#ChaparallsRelativeWinsScatterPlot").load("../../../articles/01_san_antonio_spurs/graphs/chaparrals_percentage_results.html"); 
+}
